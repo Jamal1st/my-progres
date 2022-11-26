7d0bee7a2252fb22b898a8cdaa3a378304e3e01f
Jamal1st <113001956+Jamal1st@users.noreply.github.com>
2022-11-26 23:43:58 +0700

    Add files via upload

---
+++ DASPRO LOOPING/loop11.c/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Input = ";
+    cin>>x;
+    cout<<endl;
+
+    for(int i=1; i<=10; i++){
+        cout<<i<<" X "<< x << " = "<<i*x<<endl;
+    }
+
+
+
+
+    return 0;
+}
